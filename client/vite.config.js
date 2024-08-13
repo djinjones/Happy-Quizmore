3256e898280f1f2b1b73ae94d2385e1c607e3e6f
djinjones <160979104+djinjones@users.noreply.github.com>
2024-08-12 19:57:22 -0600

    fixed vite config for produciton

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const isProduction = process.env.NODE_ENV === 'production';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // specify the host
     port: 3000, // specify the port
-    open: true, 
+    open: !isProduction, 
     // strictPort: true, // if true, exit if the port is already in use
     proxy: {
       // proxy requests matching the context to the target
       '/graphql': {
-        target: 'http://localhost:3001',
+        target: isProduction
+        ? process.env.REACT_APP_GRAPHQL_ENDPOINT  // Replace with your production server URL
+        : 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       },
