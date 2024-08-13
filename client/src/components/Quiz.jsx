2e1d9c4137134faf4ecdf5c9f9833d0e1e7cb548
Rinjee <rinjigelu39@gmail.com>
2024-08-12 21:29:40 -0600

    added deatures to the quiz css

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -1,29 +1,92 @@
-
 import { useState, useEffect, useRef } from 'react';
-import styled from 'styled-components';
-import { useQuery, useMutation,  useApolloClient } from '@apollo/client';
+import styled, { keyframes } from 'styled-components';
+import { useApolloClient } from '@apollo/client';
 import { GET_QUESTIONS } from '../utils/queries';
+import rightGif from '../assets/gifs/right.gif';
+import wrongGif from '../assets/gifs/wrong.gif';
+import FireworksDisplay from './Fireworks';
 
+const slideIn = keyframes`
+  0% {
+    transform: translateX(150%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+`;
 
+const popUp = keyframes`
+  0% {
+    transform: scale(0);
+    opacity: 0;
+  }
+  50% {
+    transform: scale(1.2);
+    opacity: 1;
+  }
+  100% {
+    transform: scale(1.5);
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
 
 const QuizContainer = styled.div`
-  display:flex;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   max-width: 840px;
   margin: 0 auto;
   padding: 20px;
-  border: 1px solid #ddd;
-  border-radius: 4px;
-  background-color: #f9f9f9;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  border-radius: 8px;
+  background-color: ${(props) => props.theme.cardBackground};
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  position: relative; /* Ensure relative positioning to position scoreboard inside */
+`;
 
-  @media (max-width: 768px) {
-    padding: 10px;
-    min-with: 430px;
-  }
+const ScoreboardContainer = styled.div`
+  position: absolute;
+  top: 10px; /* Positioning from the top */
+  right: 10px; /* Positioning from the right */
+  width: 80px;
+  height: 80px;
+  background-color: ${(props) => props.theme.scoreboardBackground};
+  border-radius: 50%;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${slideIn} 0.5s ease forwards;
+  z-index: 1000; /* Ensure it appears above other content */
+`;
 
-  @media (max-width: 480px) {
-    padding: 5px;
-    min-width: 350px;
-  }
+const ScoreText = styled.div`
+  font-size: 1.2em;
+  color: ${(props) => props.theme.text};
+  font-weight: bold;
+  text-align: center;
+`;
+
+const FeedbackImage = styled.img`
+  width: 80px;
+  height: 80px;
+  position: absolute;
+  top: -100px;
+  left: 50%;
+  transform: translateX(-50%);
+  animation: ${(props) => (props.show ? popUp : fadeOut)} 1s ease forwards;
+  display: ${(props) => (props.show ? 'block' : 'none')};
 `;
 
 const ScrollButton = styled.button`
@@ -35,49 +98,44 @@ const ScrollButton = styled.button`
   border: none;
   border-radius: 50%;
   cursor: pointer;
-  padding: 10px;
+  padding: 15px;
+  z-index: 10;
+  transition: background-color 0.3s ease, transform 0.3s ease;
 
   &:hover {
     background-color: rgba(0, 0, 0, 0.7);
+    transform: scale(1.1);
   }
 `;
 
 const LeftButton = styled(ScrollButton)`
-  left: -40px;
+  left: 0px;
 `;
 
 const RightButton = styled(ScrollButton)`
-  right: -40px;
+  right: 0px;
 `;
 
 const QuestionContainer = styled.div`
-  min-height: 400px;
-  min-width: 800px;
+  display: ${(props) => (props.isVisible ? 'flex' : 'none')};
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
   padding: 20px;
-  scroll-snap-align: center;
-  background-color: ${({ iscorrect }) => 
-    iscorrect === null 
-      ? 'transparent' 
-      : iscorrect 
-      ? 'rgba(0, 255, 0, 0.3)' 
-      : 'rgba(255, 0, 0, 0.3)'
-  };
+  background-color: ${({ iscorrect, theme }) =>
+    iscorrect === null
+      ? theme.cardBackground
+      : iscorrect
+      ? 'rgba(0, 255, 0, 0.3)'
+      : 'rgba(255, 0, 0, 0.3)'};
+  border-radius: 8px;
   transition: background-color 0.3s ease-in-out;
-
-  @media (max-width: 768px) {
-    padding: 10px;
-    min-with: 430px;
-  }
-
-  @media (max-width: 480px) {
-    padding: 5px;
-    min-width: 350px;
-  }
 `;
 
 const QuestionTitle = styled.h2`
-  font-size: 1.2em;
-  color: ${(props) => props.theme.text}; /* Adjusted for theme */
+  font-size: 1.5em;
+  color: ${(props) => props.theme.text};
+  margin-bottom: 20px;
 `;
 
 const Input = styled.input`
@@ -89,43 +147,51 @@ const Input = styled.input`
   border-radius: 4px;
   background-color: ${(props) => props.theme.inputBackground};
   color: ${(props) => props.theme.text};
+  transition: border-color 0.3s ease;
+
+  &:focus {
+    border-color: ${(props) => props.theme.buttonBackground};
+    outline: none;
+  }
 `;
 
 const Image = styled.img`
   width: 100%;
   height: auto;
-  border-radius: 4px; /* Add rounded corners to the image */
-  margin-bottom: 10px; /* Add spacing between the image and options */
+  border-radius: 8px;
+  margin-bottom: 20px;
+  object-fit: cover;
 `;
 
 const SubmitButton = styled.button`
   background-color: ${(props) => props.theme.buttonBackground};
   color: ${(props) => props.theme.buttonText};
-  padding: 10px;
+  padding: 10px 20px;
   border: none;
   border-radius: 4px;
   cursor: pointer;
-  font-size: 1em;
+  font-size: 1.2em;
+  margin-top: 10px;
+  transition: background-color 0.3s ease, transform 0.3s ease;
+
   &:hover {
     background-color: ${(props) => props.theme.buttonHover};
+    transform: scale(1.05);
   }
 `;
 
-
-
-
 const Quiz = () => {
   const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answers, setAnswers] = useState({});
-  const [submitted, setSubmitted] = useState(false);
   const [result, setResult] = useState(0);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [answerStatus, setAnswerStatus] = useState({});
-  
-  
-  // for debugging only: console.log('current questions state: ', questions);
-  
+  const [submitted, setSubmitted] = useState(false);
+  const [feedbackImage, setFeedbackImage] = useState(null); // Store feedback image URL
+  const [showFeedback, setShowFeedback] = useState(false); // Control feedback image visibility
+
   const client = useApolloClient();
   const quizContainerRef = useRef(null);
 
@@ -136,15 +202,13 @@ const Quiz = () => {
           query: GET_QUESTIONS,
         });
         setQuestions(data.questions);
-       
+
         setAnswerStatus(
           data.questions.reduce((acc, question) => {
             acc[question._id] = null; // Initialize each question's status as null
             return acc;
           }, {})
         );
-
-        console.log(questions);
       } catch (err) {
         setError(err);
       } finally {
@@ -165,91 +229,86 @@ const Quiz = () => {
   };
 
   const handleScrollLeft = () => {
-    if (quizContainerRef.current) {
-      quizContainerRef.current.scrollBy({ left: -800, behavior: 'smooth' });
-    }
+    setCurrentQuestionIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : 0));
   };
 
   const handleScrollRight = () => {
-    if (quizContainerRef.current) {
-      quizContainerRef.current.scrollBy({ left: 800, behavior: 'smooth' });
-    }
+    setCurrentQuestionIndex((prevIndex) =>
+      prevIndex < questions.length - 1 ? prevIndex + 1 : prevIndex
+    );
   };
 
   const handleSubmit = (questionId, i) => {
     const userAnswer = answers[questionId];
     const question = questions.find(q => q._id === questionId);
-    const index = i+1;
+    const index = i + 1;
     const iscorrect = question && userAnswer && userAnswer.toLowerCase() === question.title.toLowerCase();
-    
+
     if (iscorrect) {
       setResult(prevResult => prevResult + 1); // Increment score if correct
-      console.log('Correct! You answered: ', userAnswer, ' for question #', index);
+      setFeedbackImage(`${rightGif}`); // Set happy GIF
     } else {
-      console.log('Incorrect! You answered: ', userAnswer, ' for question #', index)
+      setFeedbackImage(`${wrongGif}`); // Set sad GIF
     }
-  
+
+    setShowFeedback(true);
+    setTimeout(() => setShowFeedback(false), 1500); // Hide feedback image after 1.5 second
+
     setAnswerStatus(prevStatus => ({
       ...prevStatus,
       [question._id]: iscorrect !== undefined ? iscorrect : null // Update to true/false, keep null if not answered
     }));
-    
-    console.log('Submitted answer for question ID:', questionId, 'Answer:', userAnswer);
+
+    if (currentQuestionIndex < questions.length - 1) {
+      handleScrollRight();
+    } else {
+      setSubmitted(true);
+    }
   };
 
-  if (submitted) return <div>Your result is: {result}</div>;
+
   if (error) return <p>Error loading questions</p>;
-  if (loading) return <p>Loading...</p>
+  if (loading) return <p>Loading...</p>;
+  if (submitted) return <div>Your final score is: {result} / {questions.length}</div>;
 
   return (
-    <div style={{ position: 'relative' }}>
-      <LeftButton onClick={handleScrollLeft}>{'<'}</LeftButton>
+    <>
       <QuizContainer ref={quizContainerRef}>
         {questions.length > 0 ? (
           questions.map((question, index) => (
-            <QuestionContainer key={index} iscorrect={answerStatus[question._id]}>
+            <QuestionContainer key={index} isVisible={index === currentQuestionIndex} iscorrect={answerStatus[question._id]}>
               <Image src={question.url} alt="movie scene" />
               <QuestionTitle>Which movie is this?</QuestionTitle>
-              <input 
-                className='movie-answer-input' 
-                data-question-input={question._id} 
-                type='text' 
-                placeholder='movie title here' 
+              <Input
+                className='movie-answer-input'
+                data-question-input={question._id}
+                type='text'
+                placeholder='movie title here'
                 onChange={(e) => setAnswers({ ...answers, [question._id]: e.target.value })}
               />
-              <SubmitButton 
-                className='movie-answer-submit' 
-                data-question-submit={question._id} 
+              <SubmitButton
+                className='movie-answer-submit'
+                data-question-submit={question._id}
                 onClick={() => handleSubmit(question._id, index)}>
-                  Submit
+                Submit
               </SubmitButton>
             </QuestionContainer>
           ))
         ) : (
           <p>No questions available</p>
         )}
+  
+        <LeftButton onClick={handleScrollLeft} disabled={currentQuestionIndex === 0}>{'<'}</LeftButton>
+        <RightButton onClick={handleScrollRight} disabled={currentQuestionIndex === questions.length - 1}>{'>'}</RightButton>
+  
+        <ScoreboardContainer>
+          <ScoreText>{result} / {questions.length}</ScoreText>
+          <FeedbackImage src={feedbackImage} show={showFeedback} alt="feedback" />
+        </ScoreboardContainer>
       </QuizContainer>
-      <RightButton onClick={handleScrollRight}>{'>'}</RightButton>
-    </div>
+      {submitted && <FireworksDisplay/>}  {/* Display fireworks on quiz completion */}
+    </>
   );
 };
 
 export default Quiz;
-
-
-/* 
-<p>{index}</p>
- data-number={index}
-removed from inside the questionContainer
-{question.options.map((option) => (
-              <OptionLabel key={option}>
-                <input
-                  type="radio"
-                  name={question._id}
-                  value={option}
-                  onChange={() => handleChange(question._id, option)}
-                />
-                {option}
-              </OptionLabel>
-            ))}
-*/
